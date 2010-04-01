3904e288c387d7fc947dca1cdc022e8b4f6b72e5
Tom Ward <tom@popdog.net>
2010-04-01 11:08:37 +0100

    Removed configuration both specific to me, and that not required for servers (OSX specific for example)

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,9 +1,6 @@
-system_name=`uname -a`
-
 fpath=(~/.dotfiles/zsh/functions $fpath)
 
 source ~/.dotfiles/zsh/prompt.zsh
-source ~/.dotfiles/zsh/title.zsh
 source ~/.dotfiles/zsh/misc.zsh
 source ~/.dotfiles/zsh/history.zsh
 
@@ -15,19 +12,9 @@ setopt LOCAL_TRAPS # allow functions to have local traps
 alias g='git'
 alias fr='freerange'
 
-cdpath=(~ ~/Projects/apps ~/Projects/tools ~/Projects/plugins ~/Projects/sites ~/Projects/freerange)
+cdpath=(~)
 
 setopt autopushd # Use pushd for all directory changing
 
-# Load vendor specific scripts
-
-case $system_name in
-  Darwin*)
-    source ~/.dotfiles/zsh/osx/osx.zsh
-    ;;
-  *)
-    source ~/.dotfiles/zsh/linux/linux.zsh
-    ;;;
-esac
-
+source ~/.dotfiles/zsh/linux/linux.zsh
 source ~/.dotfiles/zsh/completion.zsh
